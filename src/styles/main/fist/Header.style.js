f88564955c805d7ec78cf5f16837da09629db47d
jisoung <jangjisoung0405@gmail.com>
2022-09-28 23:30:40 +0900

    Feat : 헤더 스타일 추가

---
+++ src/styles/main/fist/Header.style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 60px;
+`;
+export const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 602px;
+`;
+export const Title = styled.span`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 20px;
+  color: #242424;
+`;
