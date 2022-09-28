9703d361e05763119b5e2e45ba1bda1b2342c5b3
jisoung <jangjisoung0405@gmail.com>
2022-09-28 22:47:02 +0900

    style : @import 제거

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -1,7 +1,6 @@
-import {createGlobalStyle} from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
