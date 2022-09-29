a50d425ec20994bba758dfca50fad59e59352ef0
jisoung <jangjisoung0405@gmail.com>
2022-09-29 22:57:42 +0900

    Feat : Third Page + Style 추가

---
+++ src/styles/main/Third.style.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const BoxContainer = styled.div`
+  display: flex;
+  gap: 40px;
+`;
+export const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  width: 280px;
+  height: 290px;
+  background: #ffffff;
+  box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+  padding: 25px;
+`;
+export const Text = styled.textarea`
+  border: 0;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 20px;
+  width: 230px;
+  height: 300px;
+  resize: none;
+`;
