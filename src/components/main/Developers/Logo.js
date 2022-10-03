f1c03e459bc1a4341ce0cd6f3a5703de009702af
jikwan <jikwan0327@gmail.com>
2022-10-03 16:31:38 +0900

    Feat : 화살표 불러오기

---
+++ src/components/main/Developers/Logo.js
@@ -0,0 +1,29 @@
+export const LeftArrow = () => {
+  return (
+    <svg
+      style={{ marginTop: 12 }}
+      width="10"
+      height="16"
+      viewBox="0 0 10 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M9.2625 13.7375L3.5375 8L9.2625 2.2625L7.5 0.5L0 8L7.5 15.5L9.2625 13.7375Z" fill="#343434" />
+    </svg>
+  );
+};
+
+export const RightArrow = () => {
+  return (
+    <svg
+      style={{ marginTop: 12 }}
+      width="10"
+      height="16"
+      viewBox="0 0 10 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M0.7375 13.7375L6.4625 8L0.7375 2.2625L2.5 0.5L10 8L2.5 15.5L0.7375 13.7375Z" fill="#343434" />
+    </svg>
+  );
+};
